d044539766b8676da7d043a2db770c838e29859c
maximyunusov <maximyunusov@gmail.com>
2015-11-12 22:07:34 +0600

    hello world

--- src/main/java/org/maxur/wmodel/Launcher.java
+++ src/main/java/org/maxur/wmodel/Launcher.java
@@ -1,9 +1,42 @@
 package org.maxur.wmodel;
 
+import io.dropwizard.Application;
+import io.dropwizard.Configuration;
+import io.dropwizard.setup.Environment;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author myunusov
  * @version 1.0
  * @since <pre>04.11.2015</pre>
  */
-public class Launcher {
+public class Launcher extends Application<Configuration> {
+
+    public static void main(final String[] args) throws Exception {
+        new Launcher().run("server");
+    }
+
+    @Override
+    public void run(final Configuration configuration, final Environment environment) {
+        environment.jersey().register(new HelloWorldResource());
+    }
+
+    @Path("/hello-world")
+    @Produces(MediaType.APPLICATION_JSON)
+    public static class HelloWorldResource {
+        @GET
+        public Map<String, Object> sayHello(@QueryParam("name") String name) {
+            Map<String, Object> res = new HashMap<>();
+            res.put("content", "Hello, " + (name != null ? name : "World"));
+            return res;
+        }
+    }
+
 }
---
+++ src/test/java/org/maxur/wmodel/LauncherTest.java
@@ -0,0 +1,10 @@
+package org.maxur.wmodel;
+
+/**
+ * @author myunusov
+ * @version 1.0
+ * @since <pre>12.11.2015</pre>
+ */
+public class LauncherTest {
+
+}
